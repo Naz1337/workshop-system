7f1d2fc9e5a3cfa8c4535810dbe473d006e6950b
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-06-04 01:40:06 +0800

    router is here

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:workshop_system/firebase_options.dart';
 import 'package:provider/provider.dart'; // Import provider package
+import 'package:go_router/go_router.dart';
 
 // Import your services and repositories
 import 'services/firestore_service.dart';
@@ -43,17 +44,28 @@ void main() async {
   );
 }
 
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MyHomePage(title: 'Flutter Demo Home Page');
+      },
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'), // Example home page
     );
   }
 }
