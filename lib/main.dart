b522787a1b404e60d9f984a2bfc1afb14869a7d5
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-06-13 03:46:03 +0800

    Squashed commit of the following:
    commit dcac957c3dc9db8cfdf2a695f55bc69e601b36a8
    Author: wnrliza <135545392+wnrliza@users.noreply.github.com>
    Date:   Thu Jun 12 14:06:28 2025 +0800
    
        latest
    
    commit 72ff8349626283d364b39d518d782265b645af42
    Author: wnrliza <135545392+wnrliza@users.noreply.github.com>
    Date:   Thu Jun 12 05:51:49 2025 +0800
    
        update demo
    
    commit b1ec6e50cf1afd7be893511b1aff92a04a14f09a
    Author: wnrliza <135545392+wnrliza@users.noreply.github.com>
    Date:   Thu Jun 12 01:58:50 2025 +0800
    
        scheduleee
    
    commit 34afd7551336270b61a847264fc365f95c1a6501
    Author: wnrliza <135545392+wnrliza@users.noreply.github.com>
    Date:   Thu Jun 12 01:58:36 2025 +0800
    
        schedule
    
    commit a6153bde371fc2a4af898b7934f36a81eaac7fc3
    Author: wnrliza <135545392+wnrliza@users.noreply.github.com>
    Date:   Wed Jun 11 19:39:43 2025 +0800
    
        bb
    
    commit 0dbdf3879d7be37253180e366074a17f1bd5af84
    Author: wnrliza <135545392+wnrliza@users.noreply.github.com>
    Date:   Wed Jun 11 17:30:52 2025 +0800
    
        stash

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
@@ -66,7 +67,9 @@ void main() async {
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ScheduleRepository scheduleRepository;
+
+  const MyApp({super.key, required this.scheduleRepository});
 
   @override
   Widget build(BuildContext context) {
